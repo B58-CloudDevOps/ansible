436fd38875d03125a5b6bad09ba367ffc315c2c0
CodingManoj <CodingManoj@gmail.com>
2024-08-19 21:57:20 -0400

    Auto Push

--- init.sh
+++ init.sh
@@ -1,1 +1,1 @@
-ansible-playbook -i ${COMPONENT}-${ENV}.expense.internal, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e COMPONENT=${COMPONENT} -e ENV=${ENV}  expense.yml
+ansible-playbook -i inv-${ENV} -e ansible_user=ec2-user -e ansible_password=DevOps321 -e COMPONENT=${COMPONENT} -e ENV=${ENV}  expense.yml
